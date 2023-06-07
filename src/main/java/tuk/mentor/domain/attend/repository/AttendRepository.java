fcf7cb5ea3a9648674f9e72e27aea47e0efcccd1
MinChangJeong <wjdalsckd777@naver.com>
2023-06-07 15:09:49 +0900

    [tem] 임시 주석처리

--- src/main/java/tuk/mentor/domain/attend/repository/AttendRepository.java
+++ src/main/java/tuk/mentor/domain/attend/repository/AttendRepository.java
@@ -1,16 +1,11 @@
 package tuk.mentor.domain.attend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tuk.mentor.domain.attend.entity.Attendance;
-import tuk.mentor.domain.user.mentee.entity.Mentee;
-
-import java.util.List;
 
 @Repository
 public interface AttendRepository extends JpaRepository<Attendance, Long> {
-    @Query("SELECT count() FROM ProgramParticipation pp WHERE pp.program.id = :programId")
-    Long findParticipantByProgramId(@Param("programId") Long programId);
+//    @Query("SELECT count() FROM ProgramParticipation pp WHERE pp.program.id = :programId")
+//    Long findParticipantByProgramId(@Param("programId") Long programId);
 }
